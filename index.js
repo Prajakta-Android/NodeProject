60a80af33a4fbdc972b1bfb4fde9fcf8d6fbb4ee
Prajakta-Android <prajaktashinde003@gmail.com>
2019-07-07 20:06:55 +0530

    testing git

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ console.log('Server running at http://127.0.0.1:3000');
 // using express
 
 const express = require("express"); //  require express
-const bodyParser = require("body-parser");
-const controllerFile = require("./controller");
+const bodyParser = require("body-parser");const controllerFile = require("./controller");
 let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
